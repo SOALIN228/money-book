e94c458c845a6f5994f2480ee22c40858cdd3ac2
lml <soalin228@163.com>
2020-05-08 22:27:20 +0800

    feat: router && CategorySelect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Home from './containers/Home'
+import Create from './containers/Creat'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 class App extends Component {
   render () {
     return (
-      <div className="App">
-        <Home/>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container pb-5">
+            <Route path="/" exact component={Home}/>
+            <Route path="/create" component={Create}/>
+            <Route path="/edit/:id" component={Create}/>
+          </div>
+        </div>
+      </Router>
     )
   }
 }
---
+++ src/containers/Creat.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Create = ({ match }) => {
+  return <h1>create {match.params.id}</h1>
+}
+
+export default Create
